483261d608149100f1d7a2fd9d6fb9ced51efa4d
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2019-10-22 16:06:57 +0200

    upgrade

--- sketch.js
+++ sketch.js
@@ -1,11 +1,37 @@
-function preload(){
-  // put preload code here
+var capture;
+
+function preload() {
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight)
+  capture = createCapture(VIDEO);
+  capture.size(windowWidth, windowHeight);
+  capture.hide();
+
 }
 
 function draw() {
-  // put drawing code here
+  background('teal');
+  var myImage = capture.loadPixels();
+
+  image(myImage, 0, 0, windowWidth, windowHeight);
+  filter(INVERT);
+
+  // TEXT
+
+    var myText = "Press 'S' to save your selfie!";
+
+   drawingContext.font = "bold 40px khand";
+   drawingContext.textAlign = "center";
+   fill(255);
+   text(myText,width/2,900);
+
+
+}
+
+// save pic function
+function keyTyped() {
+  if (key == 's' || key == 'S') save('heyYou.jpg')
 }
